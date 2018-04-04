8a4e13f6e3b6c43e2e34500bb9bf110a90d3428e
Daniel Richard <drichard989@gmail.com>
2018-04-04 13:22:36 -0700

    bdfgvz
    dfgdfgs

--- index.js
+++ index.js
@@ -1,82 +1,150 @@
-/*trueFalse();
-trueFalse2(); */
-numInput();
 
+//charSwap();
+//hi();
+//threeCharToFront();
+//stringsToSentence();
+upperOrLower();
 
-var bob = new Person("bob", "johnson", "12");
+function charSwap(){
 
-var bob2 = new clonePerson(bob, "5");
-
-console.log("first object",bob.firstName+" "+bob.lastName+" "+bob.age);
-console.log("cloned object",bob.firstName+" "+bob.lastName+" "+bob.age+" "+bob2.height);
-
-function trueFalse() {
-    var txt;
-    var r = confirm("Press a button!");
-    if (r == true) {
-        txt = "You pressed OK!";
-    } else {
-        txt = "You pressed Cancel!";
+    var person = prompt("Please enter your name", "");
+    if (person == null){
+    	console.log("user pressed cancel, try again");
+    	charSwap();
     }
-    document.getElementById("demo").innerHTML = txt;
+    if (person == ""){
+    	console.log("input empty, try again");
+    	charSwap();
+    }
+    if (person != null) {
+    	console.log(person);
+    	var first = person.charAt(0);
+    	var last = person.slice(-1);
+
+    	var newPerson =last+ person.slice(1,length-1)+first;
+        console.log(newPerson)
+    }
+
+
 }
 
-function trueFalse2() {
-    var txt;
-    var r = confirm("Press a button!");
 
+function hi(){
+	flag = 0;
+    var person = prompt("Please enter your name", "");
+    if (person == null){
+    	console.log("user pressed cancel, try again");
+    	hi();
+    }
+    if (person == ""){
+    	console.log("input empty, try again");
+    	hi();
+    }
+    if (person.charAt(0)=="H" || person.charAt(1)=="i"){
+    	console.log(person);
+    	var newPerson =person.slice(3);
+    	flag = 1;
+    	console.log(newPerson);
+
+    }
+    if (flag == 0){  
+
+
+    	var newPerson ="Hi "+ person
+        console.log(newPerson)
+    }
     
-        txt = (r)? "You pressed OK!": "You pressed Cancel!";
-    
-    document.getElementById("demo").innerHTML = txt;
+
+
 }
 
-function numInput(){
-	var input = prompt("Enter a number", "");
 
-    if (input == null) {
-        alert("no text entered");
+function threeCharToFront(){
+
+    var person = prompt("Please enter your name", "");
+
+    if (person == null){
+    	console.log("user pressed cancel, try again");
+    	threeCharToFront();
     }
-    else if (isNaN(input)){
-    	alert("input is not a number");
+    if (person == ""){
+    	console.log("input empty, try again");
+    	threeCharToFront();
     }
-    else if (isNaN(input)==false){
-    	alert("input good");
+    if (person.length <3){
+    	console.log("input not long enough, try again");
+    	threeCharToFront();
     }
+
+    if (person != null ) {
+    	console.log(person);
+    	var letters = person.slice(length-3);
+    	var more = person.slice(0,length-3);
+    	var newPerson =letters+ more;
+        console.log(newPerson)
+    }
+
+
 }
-
-function Person(first, last, age) {
-    this.firstName = first;
-    this.lastName = last;
-    this.age = age;
-    this.fullName=first+" "+last;
-}
-
-function clonePerson( person, height){
-	this.firstName = person.first;
-    this.lastName = person.last;
-    this.age = person.age;
-    this.fullName=person.first+" "+person.last;
-    if (height != null){
-    	this.height=height;
-    }
-}
-
 /*
-function myFunction(string1, string2, string3) {
+Strings to Sentence - Write a JavaScript function that asks for a list of items from the
+user separated by commas. Convert this to an array without the commas or extra
+spaces. Alert the items back to the user with a single saying that is formed using
+backticks (``).
+Example:
+input from user: blue, dogs, chocolate
+output from function: My favorite color is blue, dogs are awesome, and I love chocolate!
+*/
+function stringsToSentence(){
 
-	if (string3==null){
-		string3="default";
-	}
-    return string1 + string2+string3;
-}*/
-/*
-var string= myFunction(string1, string2, string3){if (string3==null){
-		string3="default";
-	}
-    return string1 + string2+string3;}*/
-/*
-var x =(string1, string2, string3) => {if (string3==null){
-		string3="default";
-	}
-    return string1 + string2+string3;};*/
+
+    var sentence = prompt("Please enter your name", "");
+
+    if (sentence == null){
+    	console.log("user pressed cancel, try again");
+    	stringsToSentence();
+    }
+    if (sentence == ""){
+    	console.log("input empty, try again");
+    	stringsToSentence();
+    }
+
+
+    if (sentence != null ) {
+    	var arr = sentence.split(",");
+
+        console.log(arr);
+
+        console.log(`My favorsite color is ${arr[0]}, ${arr[1]} are awesome, and I love ${arr[2]}`)
+    }
+
+}
+
+
+upperOrLower(){
+	var input = prompt("Please enter your name", "");
+
+    if (input == null){
+    	console.log("user pressed cancel, try again");
+    	upperOrLower();
+    }
+    if (sentence == ""){
+    	console.log("input empty, try again");
+    	upperOrLower();
+    }
+
+    if (input.length <3){
+
+
+    }
+
+    if (input != null || input.length>3) {
+    	var arr = input.split(",");
+
+        console.log(arr);
+
+        console.log(`My favorsite color is ${arr[0]}, ${arr[1]} are awesome, and I love ${arr[2]}`)
+    }
+
+
+}
