dfbb33cb948986a779c195d74b09c11b835b8bd6
Daniel Richard <drichard989@gmail.com>
2018-04-18 20:10:18 -0700

    dfgsdfgs
    fdg

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-//
+
+
+function buttonClicked1(){
+
+	document.getElementById("one").scrollIntoView();
+	console.log("button 1 clicked");
+}
+
+function buttonClicked2(){
+
+	document.getElementById("two").scrollIntoView();
+	console.log("button 2 clicked");
+}
+
+function buttonClicked3(){
+
+	document.getElementById("three").scrollIntoView();
+	console.log("button 3 clicked");
+}
+
+function buttonClicked4(){
+
+	document.getElementById("four").scrollIntoView();
+	console.log("button 4 clicked");
+}
+
+function buttonClicked5(){
+
+	document.getElementById("five").scrollIntoView();
+	console.log("button 5  clicked");
+}
+
+function backgroundColorBlue(){
+	document.getElementById("one").style.backgroundColor='blue';
+}
+
+function backgroundColorGreen(){
+	document.getElementById("one").style.backgroundColor='green';
+}
+
+function toggleColor(){
+
+
+	var elem = document.getElementById('button');
+	var text= elem.textContent || elem.innerText;
+
+	if(text=='Click for pink!'){
+		document.getElementById("two").style.backgroundColor='pink';
+		document.getElementById('button').innerText='Click for orange!';
+		console.log("dfhdjksf");
+	}
+
+	if(text=='Click for orange!'){
+		document.getElementById("two").style.backgroundColor='orange';
+		document.getElementById('button').innerText='Click for pink!';
+		console.log("dfhdjksf");
+	}
+
+}
+
