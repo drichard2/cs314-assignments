ccea969511f6dea3dcfb27454fdbed2d2daa8524
Daniel Richard <drichard989@gmail.com>
2018-05-06 13:30:20 -0700

    bmnv bmn
    hjkhjkbjkmn,

--- food.js
+++ food.js
@@ -1,11 +1,20 @@
 function sports(){
 	var list = document.getElementById("sportslist");
+	var list2=document.getElementById("foodlist");
+	var list3 = document.getElementById("outdoorlist");
+	list3.style.display="none";
+	list2.style.display="none";
 	list.style.display="";
 	console.log("tesxt");
 }
 
 function outdoors(){
 	var list = document.getElementById("outdoorlist");
+	var list2=document.getElementById("foodlist");
+	var list3 = document.getElementById("sportslist");
+
+	list3.style.display="none";
+	list2.style.display="none";
 	list.style.display="";
 	console.log("tesxt");
 }
@@ -13,6 +22,11 @@ function outdoors(){
 
 function food(){
 	var list = document.getElementById("foodlist");
+
+	var list2=document.getElementById("sportslist");
+	var list3 = document.getElementById("outdoorlist");
+	list3.style.display="none";
+	list2.style.display="none";
 	list.style.display="";
 	console.log("tesxt");
 }
