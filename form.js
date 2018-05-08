85a0f4412f8f07f60efde9144ac1f74bfa4484c0
Daniel Richard <drichard989@gmail.com>
2018-05-07 19:57:37 -0700

    kjnb
    kn

--- form.js
+++ form.js
@@ -98,7 +98,7 @@ function resetForm(){
 
 function checkEmail( email) {
 
-    
+    //i found this online, I have no idea how it works
  	var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
     return pattern.test(email);
 }
